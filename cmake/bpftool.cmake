3611b8f9607843d061f27add1f73d936490c00df
harry.gao <1814593561@qq.com>
2023-11-08 23:21:11 +0800

    init

--- cmake/bpftool.cmake
+++ cmake/bpftool.cmake
@@ -1,20 +1,17 @@
 
-set(BPFTOOL_ROOT "${PROJECT_SOURCE_DIR}/3rdparty/bpftool")
 
-if (NOT EXISTS "${BPFTOOL_ROOT}")
-    message(FATAL_ERROR "This project depends on bpfbool, please use the following command to download 3rdparty/bpfbool first:
-    git submodule update --init --recusive")
-endif()
 
 ExternalProject_Add(bpftool
     PREFIX bpftool
-    SOURCE_DIR ${BPFTOOL_ROOT}/src
+    GIT_REPOSITORY https://github.com/libbpf/bpftool.git
+    GIT_TAG v7.2.0
+    # SOURCE_DIR ${BPFTOOL_ROOT}/src
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND make bootstrap
-        OUTPUT=${CMAKE_CURRENT_BINARY_DIR}/bpftool/
+    BUILD_COMMAND make bootstrap -C src
+        OUTPUT=${PROJECT_SOURCE_DIR}/3rdparty/
     BUILD_IN_SOURCE TRUE
     INSTALL_COMMAND ""
     STEP_TARGETS build
 )
 
-set(BPFOBJECT_BPFTOOL_EXE ${CMAKE_CURRENT_BINARY_DIR}/bpftool/bootstrap/bpftool)
+set(BPFOBJECT_BPFTOOL_EXE ${PROJECT_SOURCE_DIR}/3rdparty/bootstrap/bpftool)
--- cmake/libbpf.cmake
+++ cmake/libbpf.cmake
@@ -1,19 +1,15 @@
-set(LIBBPF_ROOT "${PROJECT_SOURCE_DIR}/3rdparty/libbpf")
-
-if (NOT EXISTS "${LIBBPF_ROOT}")
-    message(FATAL_ERROR "This project depends on libbpf, please use the following command to download 3rdparty/libbpf first:
-    git submodule update --init --recusive")
-endif()
 
 include(ExternalProject)
 ExternalProject_Add(libbpf
     PREFIX libbpf
-    SOURCE_DIR ${LIBBPF_ROOT}/src
+    # SOURCE_DIR ${LIBBPF_ROOT}/src
+    GIT_REPOSITORY https://github.com/libbpf/libbpf.git
+    GIT_TAG v1.2.2
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND make
+    BUILD_COMMAND make -C src
         BUILD_STATIC_ONLY=1
-        OBJDIR=${CMAKE_CURRENT_BINARY_DIR}/libbpf/libbpf
-        DESTDIR=${CMAKE_CURRENT_BINARY_DIR}/libbpf
+        OBJDIR=${CMAKE_CURRENT_BINARY_DIR}/libbpf/libbpf/
+        DESTDIR=${PROJECT_SOURCE_DIR}/3rdparty/libbpf/
         INCLUDEDIR=
         LIBDIR=
         UAPIDIR=
@@ -23,5 +19,5 @@ ExternalProject_Add(libbpf
     STEP_TARGETS build
 )
 
-set(LIBBPF_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/libbpf)
-set(LIBBPF_LIBRARIES ${CMAKE_CURRENT_BINARY_DIR}/libbpf/libbpf.a)
+set(LIBBPF_INCLUDE_DIRS ${PROJECT_SOURCE_DIR}/libbpf)
+set(LIBBPF_LIBRARIES ${PROJECT_SOURCE_DIR}/libbpf/libbpf.a)
