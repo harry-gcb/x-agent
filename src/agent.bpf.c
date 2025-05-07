46a374333fc5ec498549507e0c35151c96cf7d91
harry.gao <1814593561@qq.com>
2025-05-07 12:57:11 +0000

    learn perf_event from parca-dev

--- src/agent.bpf.c
+++ src/agent.bpf.c
@@ -1,20 +1,28 @@
-#include "agent.h"
+#include <vmlinux.h>
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
+#include "agent.h"
 
-struct config_t {
-    bool debug;
-};
-const volatile struct config_t g_config = {};
-/*============================= INTERNAL STRUCTS ============================*/
-typedef struct unwind_tables_key_t {
-    int pid;
-    int shard;
-} unwind_tables_key_t;
+const char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
+const volatile bool config_verbose = false;
 /*============================== MACROS =====================================*/
+// Number of frames to walk per tail call iteration.
+#define MAX_STACK_DEPTH_PER_PROGRAM 15
+// Size of the unwind table.
+#define MAX_UNWIND_TABLE_SIZE 250 * 1000
+// Size of the `<(pid, shard_id), unwind_table>` mapping. Determines how many
+// processes we can unwind.
+#define MAX_PID_MAP_SIZE 100
+
+// Stack walking methods.
+enum stack_walking_method {
+    STACK_WALKING_METHOD_FP = 0,
+    STACK_WALKING_METHOD_DWARF = 1,
+};
+
 #define BPF_MAP(_name, _type, _key_type, _value_type, _max_entries) \
     struct {                                                        \
         __uint(type, _type);                                        \
@@ -26,10 +34,68 @@ typedef struct unwind_tables_key_t {
 #define BPF_HASH(_name, _key_type, _value_type, _max_entries)   \
     BPF_MAP(_name, BPF_MAP_TYPE_HASH, _key_type, _value_type, _max_entries)
 
+#define DEFINE_COUNTER(__func__name)                            \
+static void BUMP_##__func__name() {                             \
+    u32 *c = bpf_map_lookup_elem(&percpu_stats, &__func__name); \
+    if (c != NULL) {                                            \
+        *c += 1;                                                \
+    }                                                           \
+}
+
+/*============================= INTERNAL STRUCTS ============================*/
+typedef struct unwind_tables_key_t {
+    int pid;
+    int shard;
+} unwind_tables_key_t;
+
+typedef struct stack_unwind_row_t {
+    __u64 pc;
+    __u16 __reserved_do_not_use;
+    __u8 cfa_type;
+    __u8 rbp_type;
+    __s16 cfa_offset;
+    __s16 rbp_offset;
+} stack_unwind_row_t;
+
+typedef struct stack_unwind_table_t {
+    __u64 low_pc;
+    __u64 high_pc;
+    __u64 table_len;
+    __u64 __explicit_padding;
+    stack_unwind_row_t rows[MAX_UNWIND_TABLE_SIZE];
+} stack_unwind_table_t;
+
+
+// We reached main.
+__u32 UNWIND_SUCCESS = 1;
+// Partial stack was retrieved.
+__u32 UNWIND_TRUNCATED = 2;
+// An (unhandled) dwarf expression was found.
+__u32 UNWIND_UNSUPPORTED_EXPRESSION = 3;
+// Any other error, such as failed memory reads.
+// TODO(javierhonduco): split this error into subtypes.
+__u32 UNWIND_CATCHALL_ERROR = 4;
+// Errors that should never happen.
+__u32 UNWIND_SHOULD_NEVER_HAPPEN_ERROR = 5;
+// PC not in table (Kernel PC?).
+__u32 UNWIND_PC_NOT_COVERED_ERROR = 6;
+// Keep track of total samples.
+__u32 UNWIND_SAMPLES_COUNT = 7;
+
+
 /*================================ MAPS =====================================*/
 BPF_HASH(debug_pids, int, u8, 32);
 BPF_HASH(unwind_tables, unwind_tables_key_t, stack_unwind_table_t, MAX_PID_MAP_SIZE);
 
+BPF_MAP(percpu_stats, BPF_MAP_TYPE_PERCPU_ARRAY, u32, u32, 10);
+
+/*=========================== HELPER FUNCTIONS ==============================*/
+DEFINE_COUNTER(UNWIND_SUCCESS);
+DEFINE_COUNTER(UNWIND_TRUNCATED);
+DEFINE_COUNTER(UNWIND_UNSUPPORTED_EXPRESSION);
+DEFINE_COUNTER(UNWIND_SHOULD_NEVER_HAPPEN_ERROR);
+DEFINE_COUNTER(UNWIND_CATCHALL_ERROR);
+DEFINE_COUNTER(UNWIND_PC_NOT_COVERED_ERROR);
 
 
 static __always_inline bool is_debug_enabled_for_pid(int pid) {
@@ -49,6 +115,32 @@ static __always_inline bool has_unwind_information(pid_t pid) {
     return false;
 }
 
+static __always_inline void show_row(stack_unwind_table_t *unwind_table, index) {
+#if 0
+    u64 pc = unwind_table->rows[index].pc;
+    u16 cfa_type = unwind_table->rows[index].cfa_type;
+    s16 cfa_offset = unwind_table->rows[index].cfa_offset;
+    s16 rbp_offset = unwind_table->rows[index].rbp_offset;
+
+    bpf_printk("~ %d entry. Loc: %llx, CFA reg: %d Offset: %d, $rbp %d", index,
+               pc, cfa_type, cfa_offset, rbp_offset);
+#endif
+}
+
+static void bump_samples() {
+    u32 *c = bpf_map_lookup_elem(&percpu_stats, &UNWIND_SAMPLES_COUNT);
+    if (c != NULL) {
+        *c += 1;
+        if (*c % 50 == 0) {
+            unwind_print_stats();
+        }
+    }
+}
+
+static __always_inline int walk_user_stacktrace(struct bpf_perf_event_data *ctx) {
+    bmp_samples();
+}
+
 SEC("perf_event")
 int profile_cpu(struct bpf_perf_event_data *ctx) {
     u64 pid_tgid = bpf_get_current_pid_tgid();
@@ -59,16 +151,20 @@ int profile_cpu(struct bpf_perf_event_data *ctx) {
         return 0;
     }
 
-    if (g_config.debug) {
+    if (config_verbose) {
         bpf_printk("debug mode enabled, make sure you specified process name");
         if (!is_debug_enabled_for_pid(user_tgid)) {
             return 0;
         }
     }
+
     bool has_unwind_info = has_unwind_information(user_pid);
     if (!has_unwind_info) {
+        #if 0
         add_stacks(ctx, pid_tgid, STACK_WALKING_METHOD_FP, NULL);
+        #endif
     } else {
+        #if 0
         stack_unwind_table_t *unwind_table = find_unwind_table(user_pid, ctx->regs.ip);
         if (unwind_table == NULL) {
             bpf_printk("IP not covered. In kernel space / bug? IP %llx)", ctx->regs.ip);
@@ -87,6 +183,9 @@ int profile_cpu(struct bpf_perf_event_data *ctx) {
         show_row(unwind_table, last_idx);
         bpf_printk("pid %d tgid %d", user_pid, user_tgid);
         walk_user_stacktrace(ctx);
+        #endif
     }
+
     return 0;
 }
+
--- src/agent.c
+++ src/agent.c
@@ -1,3 +1,224 @@
-int main(int argc, char *argv[]) {
+// SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause)
+/* Copyright (c) 2022 Facebook */
+#include <argp.h>
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <getopt.h> 
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/syscall.h>
+#include <sys/sysinfo.h>
+#include <linux/perf_event.h>
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
+
+#include "agent.skel.h"
+#include "agent.h"
+
+static struct config_t {
+    bool verbose;
+    bool sw_event;
+} config = {
+    .verbose = false,
+    .sw_event = false,
+};
+
+const char *argp_program_version = "agent 0.1";
+const char *argp_program_bug_address = "https://github.com/harry-gcb/x-agent";
+const char argp_program_doc[] =
+"Summarize off-CPU time by stack trace.\n"
+"\n"
+"USAGE: agent [--help] [-p PID | -u | -k] [-m MIN-BLOCK-TIME] "
+"[-M MAX-BLOCK-TIME] [--state] [--perf-max-stack-depth] [--stack-storage-size] "
+"[duration]\n"
+"EXAMPLES:\n"
+"    agent             # trace off-CPU stack time until Ctrl-C\n"
+"    agent 5           # trace for 5 seconds only\n"
+"    agent -m 1000     # trace only events that last more than 1000 usec\n"
+"    agent -M 10000    # trace only events that last less than 10000 usec\n"
+"    agent -p 185,175,165 # only trace threads for PID 185,175,165\n"
+"    agent -t 188,120,134 # only trace threads 188,120,134\n"
+"    agent -u          # only trace user threads (no kernel)\n"
+"    agent -k          # only trace kernel threads (no user)\n";
+
+static const struct argp_option opts[] = {
+    { "pid", 'p', "PID", 0, "Trace these PIDs only, comma-separated list", 0 },
+    { "tid", 't', "TID", 0, "Trace these TIDs only, comma-separated list", 0 },
+    { "verbose", 'v', NULL, 0, "Verbose debug output", 0 },
+    { "sw-event", 's', NULL, 0, "use software event for triggering stack trace capture", 0},
+    { NULL, 'h', NULL, OPTION_HIDDEN, "Show the full help", 0 },
+    {},
+};
+
+static error_t parse_arg(int key, char *arg, struct argp_state *state)
+{
+    static int pos_args;
+    int ret;
+
+    switch (key) {
+    case 'h':
+        argp_state_help(state, stderr, ARGP_HELP_STD_HELP);
+        break;
+    case 'v':
+        config.verbose = true;
+        break;
+    case 's':
+        config.sw_event = true;
+        break;
+    default:
+        return ARGP_ERR_UNKNOWN;
+    }
     return 0;
+}
+
+static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+{
+    if (level == LIBBPF_DEBUG && !config.verbose)
+        return 0;
+    return vfprintf(stderr, format, args);
+}
+/*
+ * This function is from libbpf, but it is not a public API and can only be
+ * used for demonstration. We can use this here because we statically link
+ * against the libbpf built from submodule during build.
+ */
+extern int parse_cpu_mask_file(const char *fcpu, bool **mask, int *mask_sz);
+
+static long perf_event_open(struct perf_event_attr *hw_event, pid_t pid, int cpu, int group_fd,
+                unsigned long flags)
+{
+    int ret;
+
+    ret = syscall(__NR_perf_event_open, hw_event, pid, cpu, group_fd, flags);
+    return ret;
+}
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    int err = 0;
+    static const struct argp argp = {
+        .options = opts,
+        .parser = parse_arg,
+        .doc = argp_program_doc,
+    };
+
+    err = argp_parse(&argp, argc, argv, 0, NULL, NULL);
+    if (err) {
+        return err;
+    }
+
+    struct agent_bpf *obj = NULL;;
+
+    const char *online_cpus_file = "/sys/devices/system/cpu/online";
+    int freq = 1, pid = -1, cpu;
+
+    struct perf_event_attr attr;
+    struct bpf_link **links = NULL;
+
+    int num_cpus, num_online_cpus;
+    int *pefds = NULL, pefd;
+
+    bool *online_mask = NULL;
+
+    err = parse_cpu_mask_file(online_cpus_file, &online_mask, &num_online_cpus);
+    if (err) {
+        fprintf(stderr, "Fail to get online CPU numbers: %d\n", err);
+        goto cleanup;
+    }
+
+    num_cpus = libbpf_num_possible_cpus();
+    if (num_cpus <= 0) {
+        fprintf(stderr, "Fail to get the number of processors\n");
+        err = -1;
+        goto cleanup;
+    }
+
+    libbpf_set_print(libbpf_print_fn);
+
+    obj = agent_bpf__open();
+    if (!obj) {
+        fprintf(stderr, "failed to open BPF object\n");
+        return 1;
+    }
+    obj->rodata->config_verbose = config.verbose;
+    /* initialize global data (filtering options) */
+    // obj->rodata->user_threads_only = env.user_threads_only;
+    // obj->rodata->kernel_threads_only = env.kernel_threads_only;
+    // obj->rodata->state = env.state;
+    // obj->rodata->min_block_ns = env.min_block_time;
+    // obj->rodata->max_block_ns = env.max_block_time;
+
+
+    err = agent_bpf__load(obj);
+    if (err) {
+        fprintf(stderr, "failed to load BPF programs\n");
+        goto cleanup;
+    }
+
+    pefds = malloc(num_cpus * sizeof(int));
+    for (i = 0; i < num_cpus; i++) {
+        pefds[i] = -1;
+    }
+
+    links = calloc(num_cpus, sizeof(struct bpf_link *));
+
+    memset(&attr, 0, sizeof(attr));
+    attr.type = config.sw_event ? PERF_TYPE_SOFTWARE : PERF_TYPE_HARDWARE;
+    attr.size = sizeof(attr);
+    attr.config = config.sw_event ? PERF_COUNT_SW_CPU_CLOCK : PERF_COUNT_HW_CPU_CYCLES;
+    attr.sample_freq = freq;
+    attr.freq = 1;
+
+    for (cpu = 0; cpu < num_cpus; cpu++) {
+        /* skip offline/not present CPUs */
+        if (cpu >= num_online_cpus || !online_mask[cpu])
+            continue;
+
+        /* Set up performance monitoring on a CPU/Core */
+        pefd = perf_event_open(&attr, pid, cpu, -1, PERF_FLAG_FD_CLOEXEC);
+        if (pefd < 0) {
+            if (!config.sw_event && errno == ENOENT) {
+                fprintf(stderr,
+                    "Fail to set up performance monitor on a CPU/Core.\n"
+                    "Try running the profile example with the `--sw-event` option.\n");
+            } else {
+                fprintf(stderr, "Fail to set up performance monitor on a CPU/Core.\n");
+            }
+            err = -1;
+            goto cleanup;
+        }
+        pefds[cpu] = pefd;
+
+        /* Attach a BPF program on a CPU */
+        links[cpu] = bpf_program__attach_perf_event(obj->progs.profile_cpu, pefd);
+        if (!links[cpu]) {
+            err = -1;
+            goto cleanup;
+        }
+    }
+
+    /* Wait and receive stack traces */
+    while (1) {
+        sleep(100);
+    }
+
+cleanup:
+    if (links) {
+        for (cpu = 0; cpu < num_cpus; cpu++)
+            bpf_link__destroy(links[cpu]);
+        free(links);
+    }
+    if (pefds) {
+        for (i = 0; i < num_cpus; i++) {
+            if (pefds[i] >= 0)
+                close(pefds[i]);
+        }
+        free(pefds);
+    }
+    agent_bpf__destroy(obj);
+    free(online_mask);
+    return -err;
 }
